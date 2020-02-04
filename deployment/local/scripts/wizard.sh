c21b204abdb5c5616182b19dee9917b6490040ff
Maxat <code.datum@gmail.com>
2020-02-04 17:24:00 +0600

    Wizard changed

--- deployment/local/scripts/wizard.sh
+++ deployment/local/scripts/wizard.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
 #add these variable to .env.local
 PROJECT_APP=bottle_app
 echo "determine python version to write to .pythonversion"
@@ -9,16 +8,12 @@ fi
 echo "Create virtualenv"
 python -m pip install virtualenv
 python -m venv venv
-if [[!]]
-source ${PROJECT_APP}/venv/bin/activate
-echo "Install django framework package"
-echo "Pip install requirements.local.txt"
-
-pip install -r local/requirements.txt
-mkdir bottle
-cd bottle
-python3 -m pip install virtualenv
-virtualenv venv
-#source venv/bin/activate
-#echo "bottle==0.12.8" > requirements.txt
-#echo "freeze">> requirements.txt
+source venv/bin/activate
+mkdir requirements
+touch requirements/local.txt
+pip install -r requirements/local.txt
+#Copy template to app.py
+cp app_temp.py app.py
+chmod +x run.sh
+## run app
+./run.sh
